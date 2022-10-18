e4a2d35918cc35b6f2dac06d23ab2c8e106e2813
flyboy <3183615461@qq.com>
2022-10-18 16:17:53 +0800

    Linux中makefile的使用

---
+++ demo10.18/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+#include "printf.h"
+int main(int argc, char *argv[])
+{
+    printf("hello VS code and world again!\n");
+    printf("PI =%1f\n", PI);
+    printfplus();
+    return 0;
+}
